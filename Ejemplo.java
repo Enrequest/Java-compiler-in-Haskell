8280a2aab8073823d55e20205cd621850ba2193a
Enrequest <Enrique_Silvestre28@outlook.com>
2020-11-18 12:59:03 -0400

    Added expresions

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,13 +1,19 @@
 class A {
    int a;
-   boolean m2(int a){}
+   int c;
+   boolean m2(){}
    boolean b;
    void m(int x, boolean y, int z) {
-   	a = 4;
-   	b = false;
-   	int x1=2,x2=a;
+   	as = f(3,5);
+   	as = f();
+   	as = f(3,b) + 7;
+   	a = 2 + 4 * 3;
+   	c = (2 + 4)*3;
+   	a = 5;
+   	a = b;
+   	d = f(4) < f(5) || 5 == 6;
+   	e = 4 < 5 < 6;
    }
-   int m3(int x, boolean y);
+   //int m3(int x, boolean y);
 }
-
 
--- Parser.hs
+++ Parser.hs
@@ -27,6 +27,12 @@ pParametros = ParametroFinal <$> pParametro <|> Parametros <$> pParametro <* pSe
 
 pParametros = Parametros <$> pParametro <*> succeed PNil <|> Parametros <$> pParametro <* pSeparador "," <*> pParametros
 
+{-
+pParametros = foldl f c <$> many1 (pParametro <* pSeparador "," <*> pParametros)
+       where f a b = Parametros b a
+             c = PNil
+-}
+
 pParametro = Parametro <$> pTipo <*> pIdentificador
 
 pCuerpoMetodo = Cuerpo <$> pBloque <|> CuerpoNil <$ pSeparador ";"
@@ -37,17 +43,34 @@ pBloque = pSeparador "{" *> option (Bloque <$> pDeclaracionesBloque) BloqueNil <
 
 pBloque = pSeparador "{" *> Bloque <$> many (pDeclaracionBloque) <* pSeparador "}"
 
-pDeclaracionBloque = DeclaracionVariableLocal <$> pVariableLocal <|> DeclaracionAsignacion <$> pAsignacion
+pDeclaracionBloque = DeclaracionAsignacion <$> pAsignacion
 
-pVariableLocal = VariableLocal <$> pTipo <*> pListaVariablesLocal <* pSeparador ";"
-
-pListaVariablesLocal = ListaVariablesLocal <$> pDeclaradorVariable <*> many ( pSeparador "," *> pDeclaradorVariable)
-
+{-
 pDeclaradorVariable = DeclaradorVariable <$> pIdentificador <*> option (pOperador "=" *> pInizializadorVariable) NoInizializado
+-}
+{-
+pAsignacion = Asignacion <$> pIdentificador <* pOperador "=" <*> pExpresion <* pSeparador ";"
+-}
 
-pAsignacion = Asignacion <$> pIdentificador <* pOperador "=" <*> pInizializadorVariable <* pSeparador ";"
+pAsignacion = Asignacion <$> pIdentificador <* pOperador "=" <*> pExpresionCondicional <* pSeparador ";"
 
+pExpresionCondicional = Or <$> (ExpR <$> pExpresionRelacional) <* pOperador "||" <*> pExpresionCondicional <|> And <$> (ExpR <$> pExpresionRelacional) <* pOperador "&&" <*> pExpresionCondicional <|> ExpR <$> pExpresionRelacional
+
+pExpresionRelacional = Igual <$> pExpresion <* pOperador "==" <*> pExpresionRelacional <|> NIgual <$> pExpresion <* pOperador "!=" <*> pExpresionRelacional <|> Menor <$> pExpresion <* pOperador "<" <*> pExpresionRelacional <|> Mayor <$> pExpresion <* pOperador ">" <*> pExpresionRelacional <|> Expr <$> pExpresion
+
+pExpresion  = Suma <$> pTermino <* pOperador "+" <*> pExpresion <|> Resta <$> pTermino <* pOperador "-" <*> pExpresion <|> pTermino
+
+pTermino = Producto <$> pFactor <* pOperador "*" <*> pTermino <|> pFactor
+
+pFactor = Con <$> pInteger <|> Var <$> pIdentificador <|> pFuncion <|> pSeparador "(" *> pExpresion <* pSeparador ")"
+
+pFuncion = Fun <$> pIdentificador <* pSeparador "(" <*> (option pArgumentos []) <* pSeparador ")"
+
+pArgumentos = list <$> pExpresion <*> many (pSeparador "," *> pExpresion) 
+
+{-
 pInizializadorVariable = InizializadorLiteral <$> pLiteral <|> InizializadorIdentificador <$> pIdentificador
+-}
 
 pLiteral = LiteralEntero <$> pInteger <|> LiteralBoolean <$> pBoolean
 
@@ -83,23 +106,52 @@ data CuerpoMetodo = Cuerpo Bloque | CuerpoNil
 
 data Bloque = Bloque [DeclaracionBloque]
              deriving Show
-             
+{-             
 data DeclaracionBloque = DeclaracionVariableLocal VariableLocal
                        | DeclaracionAsignacion Asignacion
                      deriving Show
+-}
+
+data DeclaracionBloque = DeclaracionAsignacion Asignacion
+                     deriving Show
                      
+{-                     
 data VariableLocal = VariableLocal Tipo ListaVariablesLocal 
                    deriving Show
-                  
+                                     
 data ListaVariablesLocal = 
     ListaVariablesLocal DeclaradorVariable [DeclaradorVariable]
     deriving Show
+-}
 
 data DeclaradorVariable = DeclaradorVariable String InizializadorVariable
                         deriving Show
-
-data Asignacion = Asignacion String InizializadorVariable
+{-
+data Asignacion = Asignacion String Expresion
                  deriving Show
+-}
+data Asignacion = Asignacion String ExpresionCondicional
+                 deriving Show
+
+data ExpresionCondicional = Or ExpresionCondicional ExpresionCondicional
+                          | And ExpresionCondicional ExpresionCondicional
+                          | ExpR ExpresionRelacional
+                          deriving Show
+
+data ExpresionRelacional  = Igual Expresion ExpresionRelacional
+                          | NIgual Expresion ExpresionRelacional
+                          | Menor Expresion ExpresionRelacional
+                          | Mayor Expresion ExpresionRelacional
+                          | Expr Expresion
+                          deriving Show
+                          
+data Expresion = Con Int
+               | Var String
+               | Fun String [Expresion]
+               | Suma Expresion Expresion
+               | Resta Expresion Expresion
+               | Producto Expresion Expresion 
+               deriving Show
 
 data InizializadorVariable = InizializadorLiteral Literal
           | InizializadorIdentificador String
@@ -131,52 +183,4 @@ pTipo =   TEntero   <$ pPalabraReservada "int"
       <|> TBooleano <$ pPalabraReservada "boolean" <|> TVoid <$ pPalabraReservada "void"
       
 
-
-
-
-
-
-{-
-pDeclaracionesBloque = DeclaracionesBloque <$> pDeclaracionBloque <*> many pDeclaracionBloque
-
-pDeclaracionBloque = DeclaracionBloqueVariable<$> pDeclaracionVariable <|> DeclaracionBloque <$> pDeclaracion
-
-pDeclaracionVariable = DeclaracionVariable <$> pTipo <*> pListaVariablesLocal <* pSeparador ";"
-
-pDeclaracion = Declaracion <$> pDeclaracionExpresion <* pSeparador ";"
-
-pDeclaracionExpresion = DeclaracionExpresion <$> pAsignacion
--}
-       
-{-
-data Bloque = Bloque DeclaracionesBloque
-              | BloqueNil
-            deriving Show
-                        
-data DeclaracionesBloque = DeclaracionesBloque DeclaracionBloque [DeclaracionBloque]
-                 deriving Show
-                  
-data DeclaracionBloque = 
-                DeclaracionBloqueVariable DeclaracionVariableLocal
-              | DeclaracionBloque Declaracion
-              | FinDeclaracion
-                  deriving Show
-                                      
-data DeclaracionVariableLocal = 
-               DeclaracionVariable Tipo ListaVariablesLocal
-                         deriving Show
-
-data ListaVariablesLocal = 
-    ListaVariablesLocal DeclaradorVariable [DeclaradorVariable]
-    deriving Show
-    
-data DeclaradorVariable = DeclaradorVariable String InizializadorVariable
-                        deriving Show
-                                                             
-data Declaracion = Declaracion DeclaracionExpresion
-                 deriving Show
-                 
-data DeclaracionExpresion = DeclaracionExpresion Asignacion
-                      deriving Show
--}      
 
